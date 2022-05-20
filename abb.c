4d47d658f723b1d45aa341ef125fe95e5b73b101
Matias <matias.h.mamani@gmail.com>
2022-05-19 23:46:31 -0300

    19/05 Borrado siendo probado

--- abb.c
+++ abb.c
@@ -16,10 +16,9 @@ typedef struct nodo {
 struct abb {
 	nodo_t* raiz;
 	size_t cantidad;
-    	abb_comparar_clave_t cmp;
+    abb_comparar_clave_t cmp;
 	abb_destruir_dato_t destruir_dato;
 };
-
 
 struct abb_iter {
 	pila_t* pila;
@@ -34,6 +33,12 @@ void* __abb_destruir_nodo(nodo_t* nodo);
 nodo_t* __abb_buscar_nodo(const char* clave, nodo_t* nodo, nodo_t** padre, abb_comparar_clave_t cmp);
 
 void __abb_destruir_wrapper(nodo_t* nodo, abb_destruir_dato_t destruir_dato);
+
+void* __abb_borrar_sin_hijos(abb_t* arbol, nodo_t* nodo, nodo_t* padre);
+
+void* __abb_borrar_un_hijo(abb_t* arbol, nodo_t* nodo, nodo_t* padre);
+
+void* __abb_borrar_dos_hijos(abb_t* arbol, nodo_t* nodo, nodo_t* padre);
 
 /* *****************************************************************
  *                    PRIMITIVAS DEL ABB
@@ -92,7 +97,28 @@ bool abb_guardar(abb_t *arbol, const char *clave, void *dato){
 
 void* abb_borrar(abb_t *arbol, const char *clave){
     
-    return NULL;
+    nodo_t* padre = NULL;
+    nodo_t* nodo = __abb_buscar_nodo(clave, arbol->raiz, &padre, arbol->cmp);
+
+    if(!nodo) return NULL;
+
+    void* dato = NULL;
+
+    if(!nodo->izq && !nodo->der){
+        // Nodo sin hijos
+        dato = __abb_borrar_sin_hijos(arbol, nodo, padre);
+        return dato;    
+    }
+    
+    if(nodo->izq && nodo->der){
+        // Nodo tiene dos hijos
+        dato = __abb_borrar_dos_hijos(arbol, nodo, padre);
+        return dato;
+    }
+
+    // Nodo tiene un solo hijo
+    dato = __abb_borrar_un_hijo(arbol, nodo, padre);
+    return dato;
 }
 
 void* abb_obtener(const abb_t *arbol, const char *clave){
@@ -288,4 +314,88 @@ void __abb_destruir_wrapper(nodo_t* nodo, abb_destruir_dato_t destruir_dato){
     void* dato = __abb_destruir_nodo(nodo);
 
     if(destruir_dato) destruir_dato(dato);
+}
+
+void* __abb_borrar_sin_hijos(abb_t* arbol, nodo_t* nodo, nodo_t* padre){
+    
+    // Si padre es NULL, entonces nodo a borrar es raiz (corrijo raiz)
+    // Sino, corrijo el padre
+    if(!padre){
+        arbol->raiz = NULL;   
+    } else {
+        // Valido que nodo a izq o der del padre debo actualizar
+        // Tengo que validar el puntero != NULL y que la clave coincida
+        if(padre->izq && !arbol->cmp(nodo->clave, padre->izq->clave)){
+            padre->izq = NULL;
+        } else {
+            padre->der = NULL;
+        }
+    }
+
+    arbol->cantidad--;
+
+    void* dato = __abb_destruir_nodo(nodo);
+
+    return dato;
+}
+
+void* __abb_borrar_un_hijo(abb_t* arbol, nodo_t* nodo, nodo_t* padre){
+
+    nodo_t* hijo = NULL;
+
+    if(nodo->izq){
+        hijo = nodo->izq;
+    } else {
+        hijo = nodo->der;
+    }
+
+    // Si padre es NULL, entonces nodo a borrar es raiz (corrijo raiz)
+    // Sino, corrijo el padre
+    if(!padre){
+        arbol->raiz = hijo;
+    } else {
+        // Valido que nodo a izq o der del padre debo actualizar
+        // Tengo que validar el puntero != NULL y que la clave coincida
+        if(padre->izq && !arbol->cmp(nodo->clave, padre->izq->clave)){
+            padre->izq = hijo;
+        } else {
+            padre->der = hijo;
+        }
+    }
+    
+    arbol->cantidad--;
+
+    void* dato = __abb_destruir_nodo(nodo);
+
+    return dato;
+}
+
+void* __abb_borrar_dos_hijos(abb_t* arbol, nodo_t* nodo, nodo_t* padre){
+
+    nodo_t* padre_aux = nodo;
+    nodo_t* nodo_aux = nodo->der;
+    
+    while(nodo_aux->izq){
+        padre_aux = nodo_aux;
+        nodo_aux = nodo_aux->izq;
+    }
+
+    free(nodo->clave);
+    nodo->clave = nodo_aux->clave;
+
+    void* dato = NULL;
+    void* dato_aux = NULL;
+
+    if(nodo_aux->der){
+        // Nodo aux tiene un solo hijo
+        dato_aux = __abb_borrar_un_hijo(arbol, nodo_aux, padre_aux);
+    } else {
+        // Nodo aux sin hijos
+        dato_aux = __abb_borrar_sin_hijos(arbol, nodo_aux, padre_aux);    
+    }
+
+    dato = nodo->dato;
+    nodo->dato = dato_aux;
+
+    return dato;    
 }
--- pruebas_abb.c
+++ pruebas_abb.c
@@ -14,7 +14,7 @@ static void prueba_crear_abb_vacio()
     print_test("Prueba abb la cantidad de elementos es 0", abb_cantidad(abb) == 0);
     print_test("Prueba abb obtener clave A, es NULL, no existe", !abb_obtener(abb, "A"));
     print_test("Prueba abb pertenece clave A, es false, no existe", !abb_pertenece(abb, "A"));
-    /* print_test("Prueba abb borrar clave A, es NULL, no existe", !abb_borrar(abb, "A")); */
+    print_test("Prueba abb borrar clave A, es NULL, no existe", !abb_borrar(abb, "A"));
 
     abb_destruir(abb);
 }
@@ -35,18 +35,18 @@ static void prueba_abb_insertar()
     print_test("Prueba abb obtener clave1 es valor1", abb_obtener(abb, clave1) == valor1);
     print_test("Prueba abb obtener clave1 es valor1", abb_obtener(abb, clave1) == valor1);
     print_test("Prueba abb pertenece clave1, es true", abb_pertenece(abb, clave1));
-    /* print_test("Prueba abb borrar clave1, es valor1", abb_borrar(abb, clave1) == valor1);
-    print_test("Prueba abb la cantidad de elementos es 0", abb_cantidad(abb) == 0); */
+    print_test("Prueba abb borrar clave1, es valor1", abb_borrar(abb, clave1) == valor1);
+    print_test("Prueba abb la cantidad de elementos es 0", abb_cantidad(abb) == 0);
 
     /* Inserta otros 2 valores y no los borra (se destruyen con el abb) */
     print_test("Prueba abb insertar clave2", abb_guardar(abb, clave2, valor2));
-    //print_test("Prueba abb la cantidad de elementos es 1", abb_cantidad(abb) == 1);
+    print_test("Prueba abb la cantidad de elementos es 1", abb_cantidad(abb) == 1);
     print_test("Prueba abb obtener clave2 es valor2", abb_obtener(abb, clave2) == valor2);
     print_test("Prueba abb obtener clave2 es valor2", abb_obtener(abb, clave2) == valor2);
     print_test("Prueba abb pertenece clave2, es true", abb_pertenece(abb, clave2));
 
     print_test("Prueba abb insertar clave3", abb_guardar(abb, clave3, valor3));
-    //print_test("Prueba abb la cantidad de elementos es 2", abb_cantidad(abb) == 2);
+    print_test("Prueba abb la cantidad de elementos es 2", abb_cantidad(abb) == 2);
     print_test("Prueba abb obtener clave3 es valor3", abb_obtener(abb, clave3) == valor3);
     print_test("Prueba abb obtener clave3 es valor3", abb_obtener(abb, clave3) == valor3);
     print_test("Prueba abb pertenece clave3, es true", abb_pertenece(abb, clave3));
